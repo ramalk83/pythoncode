18259a1fe1655185968c82513b2d6b986071d0af
Ram <ramalk0813@gmail.com>
2020-11-02 11:38:27 -0500

    updated

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const request=require('request');
+const request = require('request');
 const Constants = require('./config/Constants');
 const logger = require('./config/Logger');
 const sqlDS = require('./store/sqlDataStore');
-const email=require('./utils/emailDispatch');
+const email = require('./utils/emailDispatch');
 const app = express();
 var path = require('path')
 
 // const sampleRoutes = require('./routes/sampleRoutes');
-app.set("view engine", "ejs");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: 'application/*+json' }));
-app.use("/styles/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))); 
+
 // app.use(sampleRoutes);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render(__dirname + '/views/index');
+  res.render(__dirname + '/views/index');
 });
 
 app.get('/assignments', function (req, res) {
-    res.render(__dirname + '/views/assignments');
+  res.render(__dirname + '/views/assignments');
 });
 
 
@@ -36,17 +36,119 @@ app.get('/python-courses', function (req, res) {
   res.render(__dirname + '/views/courses');
 });
 
+
+// app.get('/view-course',function (req, res) {
+//   try {
+//     id=req.query.id
+//     logger.debug(id)
+//     sqlDS.listTitles().then(
+//       function (data) {
+//         // logger.debug(data)
+//         const key = 'day';
+//         function uniqueBy(arr, prop){
+//           return arr.reduce((a, d) => {
+//             if (!a.includes(d[prop])) { a.push(d[prop]); }
+//             return a;
+//           }, []);
+//         }
+//         var uniqueDays=uniqueBy(data, "day");
+//         logger.debug("******************* Exited Routes Successfully ********************************");
+//         res.render(__dirname + '/views/video_viewer', { id:id, data:data, uniqueDays:uniqueDays.length });
+//       }
+//     ).catch(function (err) {
+//       logger.debug("Promise rejection error: " + err.stack);
+//       logger.debug("******************* Exited Routes With Failure ********************************");
+//       res.sendStatus(Constants.Response.CODE_INTRNL_SERV_ERR).send(Constants.Response.DESC_INTRNL_SERV_ERR);
+//     }
+//     );
+//   } catch (e) {
+//     console.log(e)
+//     res.sendStatus(Constants.Response.CODE_INTRNL_SERV_ERR).send(Constants.Response.DESC_INTRNL_SERV_ERR);
+//   }
+// })
+
+
+app.route('/view-course')
+  .get(function (req, res) {
+    logger.debug("******************* Entered Routes ********************************")
+  try {
+    // id=req.query.id
+    // logger.debug(id)
+    sqlDS.listTitles().then(
+      function (data) {
+        // logger.debug(data)
+        const key = 'day';
+        function uniqueBy(arr, prop){
+          return arr.reduce((a, d) => {
+            if (!a.includes(d[prop])) { a.push(d[prop]); }
+            return a;
+          }, []);
+        }
+        var uniqueDays=uniqueBy(data, "day");
+        logger.debug("******************* Exited Routes Successfully ********************************");
+        res.render(__dirname + '/views/video_viewer', { data:data, uniqueDays:uniqueDays.length });
+      }
+    ).catch(function (err) {
+      logger.debug("Promise rejection error: " + err.stack);
+      logger.debug("******************* Exited Routes With Failure ********************************");
+      res.sendStatus(Constants.Response.CODE_INTRNL_SERV_ERR).send(Constants.Response.DESC_INTRNL_SERV_ERR);
+    }
+    );
+  } catch (e) {
+    console.log(e)
+    res.sendStatus(Constants.Response.CODE_INTRNL_SERV_ERR).send(Constants.Response.DESC_INTRNL_SERV_ERR);
+  }
+  })
+  .post(function (req, res) {
+    // app.post('/view-course',function (req, res) {
+    logger.debug('post invoked')
+    const videoObj = req.body
+    // const videoObj = req.query
+    logger.debug(videoObj)
+    let validationErrors = ''
+    logger.debug("validationErrors in route = " + validationErrors)
+    if (validationErrors) {
+      logger.error("invalid object")
+      res.status(Constants.Response.CODE_BAD_REQUEST).send(Constants.Response.DESC_BAD_REQUEST);
+    } else {
+      logger.debug("valid object")
+      try {
+        sqlDS.getVideo(videoObj).then(
+          function (result) {
+            logger.debug(result)
+            var id = result[0].id
+            var embed_url = result[0].embed_url
+            var title=result[0].title
+            logger.debug(id, embed_url)
+            logger.debug("******************* Exited Routes Successfully ********************************");
+            res.send({ id: id, embed_url: embed_url,title:title});
+            // res.end();
+          }
+        ).catch(function (err) {
+          logger.error("Promise rejection error: " + err.stack);
+          logger.debug("******************* Exited Routes With Failure ********************************");
+          res.status(Constants.Response.CODE_INTRNL_SERV_ERR).send(Constants.Response.DESC_INTRNL_SERV_ERR);
+        }
+        );
+      } catch (e) {
+        logger.error(e)
+        res.status(Constants.Response.CODE_INTRNL_SERV_ERR).send(Constants.Response.DESC_INTRNL_SERV_ERR);
+      }
+    }
+  })
+
+
 app.get('/recaptcha', function (req, res) {
   res.render(__dirname + '/views/recaptcha');
 });
 
 app.get('/thankyou', function (req, res) {
   logger.debug(req.headers.referrer)
-  if (req.headers.referer!=undefined && req.headers.referer.toLowerCase().indexOf("/assignments") >= 0){
+  if (req.headers.referer != undefined && req.headers.referer.toLowerCase().indexOf("/assignments") >= 0) {
     logger.debug('thank you page invoked')
     res.render(__dirname + '/views/thankyou');
   }
-  else{
+  else {
     logger.debug(req.headers.referer)
     res.redirect('/');
   }
@@ -54,119 +156,119 @@ app.get('/thankyou', function (req, res) {
 });
 
 
-app.post('/subscribe',function (req, res) {
+app.post('/subscribe', function (req, res) {
   console.log(req.body.name)
 
   // console.log(req.body.captcha)
-if (req.body.captcha===undefined || req.body.captcha==='' || req.body.captcha===null){
+  if (req.body.captcha === undefined || req.body.captcha === '' || req.body.captcha === null) {
 
-  return (res.json({'success':false,'msg':'Please select capcha'}))
-}
-//secret key
-const secretKey='6LfYX8QZAAAAAFOp_VwVFAVHmXXKesAWucXnmrDy'
+    return (res.json({ 'success': false, 'msg': 'Please select capcha' }))
+  }
+  //secret key
+  const secretKey = Constants.Recaptcha.SERVER_KEY
 
-//verify URL
-const verifyURL=`https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`
+  //verify URL
+  const verifyURL = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`
 
-//make request to verify url
-request(verifyURL, (err,response,body)=>{ 
-body=JSON.parse(body)
-// console.log(body)
-if(body.success!==undefined && !body.success){
+  //make request to verify url
+  request(verifyURL, (err, response, body) => {
+    body = JSON.parse(body)
+    // console.log(body)
+    if (body.success !== undefined && !body.success) {
 
-  return res.json({'success':false,'msg':'failed capcha verification'})
-}
-return res.json({'success':true,'msg':'capcha verified'})
-})
+      return res.json({ 'success': false, 'msg': 'failed capcha verification' })
+    }
+    return res.json({ 'success': true, 'msg': 'capcha verified' })
+  })
 
 })
 
 
 
 app.post('/message', async (req, res) => {
-    logger.debug("******************* Entered Routes ********************************");
-    const messageObj = req.body
-    logger.debug("from static instance is " + messageObj)
-  
-    let validationErrors = ''
-    // logger.debug("validationErrors in route = " + validationErrors)
-    if (validationErrors) {
-      logger.error("invalid object")
-      res.status(Constants.Response.CODE_BAD_REQUEST).send(Constants.Response.DESC_BAD_REQUEST);
-    } else {
-      logger.debug("valid object")
-      try {
-        sqlDS.createMessage(messageObj).then(
-          
-          function (result) {
-            logger.debug("******************* Exited Routes Successfully ********************************");
-            res.status(Constants.Response.CODE_SUCCESS).send(Constants.Response.CODE_SUCCESS);
+  logger.debug("******************* Entered Routes ********************************");
+  const messageObj = req.body
+  logger.debug("from static instance is " + messageObj)
 
-            // res.sendStatus(Constants.Response.CODE_SUCCESS)
-          }
-        ).catch(function (err) {
-          logger.error("Promise rejection error: " + err.stack);
-          logger.debug("******************* Exited Routes With Failure ********************************");
-          res.status(Constants.Response.CODE_INTRNL_SERV_ERR).send(Constants.Response.DESC_INTRNL_SERV_ERR);
+  let validationErrors = ''
+  // logger.debug("validationErrors in route = " + validationErrors)
+  if (validationErrors) {
+    logger.error("invalid object")
+    res.status(Constants.Response.CODE_BAD_REQUEST).send(Constants.Response.DESC_BAD_REQUEST);
+  } else {
+    logger.debug("valid object")
+    try {
+      sqlDS.createMessage(messageObj).then(
+
+        function (result) {
+          logger.debug("******************* Exited Routes Successfully ********************************");
+          res.status(Constants.Response.CODE_SUCCESS).send(Constants.Response.CODE_SUCCESS);
+
+          // res.sendStatus(Constants.Response.CODE_SUCCESS)
         }
-        );
-      } catch (e) {
-        logger.error(e)
+      ).catch(function (err) {
+        logger.error("Promise rejection error: " + err.stack);
+        logger.debug("******************* Exited Routes With Failure ********************************");
         res.status(Constants.Response.CODE_INTRNL_SERV_ERR).send(Constants.Response.DESC_INTRNL_SERV_ERR);
       }
+      );
+    } catch (e) {
+      logger.error(e)
+      res.status(Constants.Response.CODE_INTRNL_SERV_ERR).send(Constants.Response.DESC_INTRNL_SERV_ERR);
     }
-  });
-  
+  }
+});
 
 
-  app.post('/assignments', async (req, res) => {
-    logger.debug("******************* Entered Routes ********************************"); 
-    const assignmentObj = req.body
-    logger.debug("from static instance is " + assignmentObj.answer1)
-  
-    let validationErrors = ''
-    // logger.debug("validationErrors in route = " + validationErrors)
-    if (validationErrors) {
-      logger.error("invalid object")
-      res.status(Constants.Response.CODE_BAD_REQUEST).send(Constants.Response.DESC_BAD_REQUEST);
-    } else {
-      logger.debug("valid object")
-      try {
-        logger.debug("******************* Dispatch email invoked ********************************");     
-        email.dispatchEmail(assignmentObj).then(
-          function (result) {
-          logger.debug("******************* Email dispatched ********************************");  
-          }  
-        ).catch(function (err) {
-          logger.error("Promise rejection error: " + err.stack);
-          logger.debug("******************* Exited Routes With Failure ********************************");
-          res.status(Constants.Response.CODE_INTRNL_SERV_ERR).send(Constants.Response.DESC_INTRNL_SERV_ERR);
-        })
-        sqlDS.createAssignment(assignmentObj).then(
-          function (result) {
-            logger.debug("******************* Exited Routes Successfully ********************************");
-            return (res.json({'status':200, 'url':'/thankyou'}))
-            // return res.send({status:200, result: 'redirect', url:__dirname + '\\views\\thankyou'})
-          }
-        ).catch(function (err) {
-          logger.error("Promise rejection error: " + err.stack);
-          logger.debug("******************* Exited Routes With Failure ********************************");
-          res.status(Constants.Response.CODE_INTRNL_SERV_ERR).send(Constants.Response.DESC_INTRNL_SERV_ERR);
+
+app.post('/assignments', async (req, res) => {
+  logger.debug("******************* Entered Routes ********************************");
+  const assignmentObj = req.body
+  logger.debug("from static instance is " + assignmentObj.answer1)
+
+  let validationErrors = ''
+  // logger.debug("validationErrors in route = " + validationErrors)
+  if (validationErrors) {
+    logger.error("invalid object")
+    res.status(Constants.Response.CODE_BAD_REQUEST).send(Constants.Response.DESC_BAD_REQUEST);
+  } else {
+    logger.debug("valid object")
+    try {
+      logger.debug("******************* Dispatch email invoked ********************************");
+      email.dispatchEmail(assignmentObj).then(
+        function () {
+          logger.debug("******************* Email dispatched ********************************");
         }
-        );
-      } catch (e) {
-        logger.error(e)
+      ).catch(function (err) {
+        logger.error("Promise rejection error: " + err.stack);
+        logger.debug("******************* Errors with email dispatch ********************************");
+        // res.status(Constants.Response.CODE_INTRNL_SERV_ERR).send(Constants.Response.DESC_INTRNL_SERV_ERR);
+      })
+      sqlDS.createAssignment(assignmentObj).then(
+        function (result) {
+          logger.debug("******************* Exited Routes Successfully ********************************");
+          return (res.json({ 'status': 200, 'url': '/thankyou' }))
+        }
+      ).catch(function (err) {
+        logger.error("Promise rejection error: " + err.stack);
+        logger.debug("******************* Exited Routes With Failure ********************************");
         res.status(Constants.Response.CODE_INTRNL_SERV_ERR).send(Constants.Response.DESC_INTRNL_SERV_ERR);
-      }
+      });
+      //   })
     }
-  });
+    catch (e) {
+      logger.error(e)
+      res.status(Constants.Response.CODE_INTRNL_SERV_ERR).send(Constants.Response.DESC_INTRNL_SERV_ERR);
+    }
+  }
+});
 
 app.use(function (req, res) {
   res.render(__dirname + '/views/error');
 });
 
 app.listen(Constants.Server.PORT, () => {
-    console.log("Node Server started at " + Constants.Server.PORT + ", waiting for requests....");
+  console.log("Node Server started at " + Constants.Server.PORT + ", waiting for requests....");
 
 
 });
--- store/sqlDataStore.js
+++ store/sqlDataStore.js
@@ -1,13 +1,15 @@
 const logger = require('../config/Logger');
 const mscf = require('./mySQLConnectionFactory');
 
+// async function createcon() {
+// await createConnection(mscf.pool)}
 
 
 exports.createMessage = function (messageObj) {
   logger.debug("################### Entered SQLDataStore ################################");
-  logger.debug(messageObj.name, messageObj.email,messageObj.message)
+  logger.debug(messageObj.name, messageObj.email, messageObj.message)
   let SQL = "INSERT into codepannu.messages (name, email, message, created_on) values (?)";
-  let DATA = [[String(messageObj.name), String(messageObj.email),String(messageObj.message),new Date()]];
+  let DATA = [[String(messageObj.name), String(messageObj.email), String(messageObj.message), new Date()]];
 
   return new Promise(function (resolve, reject) {
     // var con = mscf.getSQLConnection();
@@ -17,7 +19,9 @@ exports.createMessage = function (messageObj) {
         reject(new Error('Ooops, something broke!'));
       } else {
         var x = con.query(SQL, DATA, function (err, result, fields) {
-          logger.debug("################### Entered SQL ################################");
+          logger.debug("################### releasing connection ################################");
+          con.release();
+          logger.debug("################### SQL Executed ################################");
           logger.debug(x.sql);
           if (err) {
             logger.error('Error:- ' + err.stack);
@@ -37,9 +41,9 @@ exports.createMessage = function (messageObj) {
 
 exports.createAssignment = function (assignmentObj) {
   logger.debug("################### Entered SQLDataStore ################################");
-  logger.debug(assignmentObj.name, assignmentObj.email,assignmentObj.level, assignmentObj.day, assignmentObj.answer1)
+  logger.debug(assignmentObj.name, assignmentObj.email, assignmentObj.level, assignmentObj.day, assignmentObj.answer1)
   let SQL = "INSERT into codepannu.assignments (name, email, level, day, answer1, created_on) values (?)";
-  let DATA = [[String(assignmentObj.name), String(assignmentObj.email),String(assignmentObj.level),assignmentObj.day,String(assignmentObj.answer1), new Date()]];
+  let DATA = [[String(assignmentObj.name), String(assignmentObj.email), String(assignmentObj.level), assignmentObj.day, String(assignmentObj.answer1), new Date()]];
 
   return new Promise(function (resolve, reject) {
     // var con = mscf.getSQLConnection();
@@ -49,7 +53,9 @@ exports.createAssignment = function (assignmentObj) {
         reject(new Error('Ooops, something broke!'));
       } else {
         var x = con.query(SQL, DATA, function (err, result, fields) {
-          logger.debug("################### Entered SQL ################################");
+          logger.debug("################### releasing connection ################################");
+          con.release();
+          logger.debug("################### SQL Executed ################################");
           logger.debug(x.sql);
           if (err) {
             logger.error('Error:- ' + err.stack);
@@ -64,5 +70,79 @@ exports.createAssignment = function (assignmentObj) {
     });
   });
 };
+
+
+exports.getVideo = function (videoObj) {
+  logger.debug("################### Entered SQLDataStore ################################");
+  logger.debug(videoObj.id)
+  DATA=[]
+    var SQL = "SELECT * FROM codepannu.videos WHERE id=?";
+    if (videoObj.func=='prevClick'){
+      if (videoObj.id==1) var DATA = [parseInt(videoObj.id)];  
+      else var DATA = [parseInt(videoObj.id)-1];    
+    }
+    else if (videoObj.func=='nextClick'){
+      logger.debug('next click invoked')
+      var DATA = [parseInt(videoObj.id)+1];
+    }
+    else{
+      var DATA = [parseInt(videoObj.id)]
+    }
+    logger.debug('printing the data')
+    logger.debug(DATA)
+
+  return new Promise(function (resolve, reject) {
+    mscf.pool.getConnection(function (err, con) {
+      if (err) {
+        logger.error('Error:- ' + err.stack);
+        reject(new Error('Ooops, something broke!'));
+      } else {
+        con.query(SQL, DATA, function (err, result, fields) {
+          logger.debug("################### releasing connection ################################");
+          con.release();
+          logger.debug("################### SQL Executed ################################");
+          // logger.debug(x.sql);
+          if (err) {
+            logger.error('Error:- ' + err.stack);
+            reject(new Error('There is an error'));
+          } else {
+            logger.debug(result.affectedRows);
+            logger.debug("################### Exited SQLDataStore ################################");
+            resolve(result);
+          }
+        });
+      }
+    });
+  });
+};
+
+exports.listTitles = function () {
+  logger.debug("################### Entered SQLDataStore ################################");
+    var SQL = "SELECT id, day, title,embed_url FROM codepannu.videos";
+  return new Promise(function (resolve, reject) {
+    mscf.pool.getConnection(function (err, con) {
+      if (err) {
+        logger.error('Error:- ' + err.stack);
+        reject(new Error('Ooops, something broke!'));
+      } else {
+        con.query(SQL, function (err, result, fields) {
+          logger.debug("################### releasing connection ################################");
+          con.release();
+          logger.debug("################### SQL Executed ################################");
+          // logger.debug(x.sql);
+          if (err) {
+            logger.error('Error:- ' + err.stack);
+            reject(new Error('There is an error'));
+          } else {
+            logger.debug(result.affectedRows);
+            logger.debug("################### Exited SQLDataStore ################################");
+            resolve(result);
+          }
+        });
+      }
+    });
+  });
+};
+
 
 
